7fd9b5fa42c0042e2b3b2a3cc73e58f6e1d8859f
Alejandro Velez <alejovg0911@gmail.com>
2019-10-16 14:42:03 -0500

    v9

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
-export const URL_SERVICIOS = 'http://localhost/gestionUsuarios/services/'
-// export const URL_SERVICIOS = 'http://localhost:8080/gestionUsuarios/services/'
+// export const URL_SERVICIOS = 'http://localhost/gestionUsuarios/services/'
+export const URL_SERVICIOS = 'http://localhost:8080/gestionUsuarios/services/'
